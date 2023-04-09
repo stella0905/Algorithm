2b85f70f0e3985764592e958c904aeedfd28b31c
stella0905 <109677566+stella0905@users.noreply.github.com>
2023-04-09 23:00:56 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.76 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+  let result = [arr[0]]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[i - 1]) {
+      result.push(arr[i])
+    }
+  }
+  return result;
+}
