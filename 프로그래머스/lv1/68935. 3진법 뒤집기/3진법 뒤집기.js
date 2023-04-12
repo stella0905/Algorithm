792e359a6df3c92549491dd19383855f0d2197c1
stella0905 <109677566+stella0905@users.noreply.github.com>
2023-04-12 22:09:44 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,12 +1,9 @@
 function solution(n) {
   let result = []
   let sum = n
-  while (sum >= 3) {
+  while (sum > 0) {
     result.push(sum % 3)
     sum = parseInt(sum / 3)
-  }
-  if (sum < 3) {
-    result.push(sum)
   }
   return result.map((a, index) => a * (3 ** (result.length - 1 - index)))
     .reduce((a, b) => a + b);
