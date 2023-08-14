e82573fdc0e729db008c43b29d3422c55507f2d0
stella0905 <109677566+stella0905@users.noreply.github.com>
2023-08-14 22:14:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 58.33 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,20 +1,25 @@
-
 function solution(participant, completion) {
-  const myMap = new Map();
+  let map = new Map();
   for (let i = 0; i < participant.length; i++) {
-    if (!myMap.has(participant[i])) {
-      myMap.set(participant[i], 1);
+    if (!map.has(participant[i])) {
+      map.set(participant[i], 1);
     } else {
-      myMap.set(participant[i], myMap.get(participant[i]) + 1)
+      map.set(participant[i], map.get(participant[i]) + 1);
     }
   }
-
-  for (com of completion) {
-    myMap.set(com, myMap.get(com) - 1)
-  }
-  for (const key of myMap.keys()) {
-    if (myMap.get(key) === 1) {
-      return key
+  for (let i = 0; i < completion.length; i++) {
+    if (map.has(completion[i])) {
+      map.set(completion[i], map.get(completion[i]) - 1);
     }
   }
+  let result = "";
+  for (let key of map.keys()) {
+    if (map.get(key) === 1) {
+      result = key;
+    }
+  }
+  return result;
 }
+console.log(
+  solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+);
